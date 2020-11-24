b75f06dd42b14ca46c132dea79a11c4d6c24e5f8
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-11-24 13:58:00 +1300

    WIP

--- src/elementPlugins/shortText/src/ApplicationView.tsx
+++ src/elementPlugins/shortText/src/ApplicationView.tsx
@@ -1,6 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { Form } from 'semantic-ui-react'
 import { ApplicationViewProps } from '../../types'
+import config from '../pluginConfig.json'
+import evaluateExpression from '@openmsupply/expression-evaluator'
+
+const dynamicFieldNames = config.dynamicParameterFields
+
+const evaluateDynamicFields = async (parameters: any) => {
+  const dynamicFields = Object.keys(parameters).filter((key: any) => {
+    key in dynamicFieldNames
+  })
+  console.log('dynamicFields', dynamicFields)
+  const dynamicExpressions = dynamicFields.map((field: any) => {
+    return parameters[field]
+  })
+  console.log('dynamicExpressions', dynamicExpressions)
+
+  const promiseArray: Promise<any>[] = []
+  dynamicExpressions.forEach((exp: any) => {
+    promiseArray.push(evaluateExpression(exp))
+  })
+  const evaluatedParameters = await Promise.all(promiseArray)
+  return zipArraysToObject(dynamicFields, evaluatedParameters)
+}
+
+// Build an object from an array of field names and an array of values
+const zipArraysToObject = (variableNames: any[], variableValues: any[]) => {
+  const createdObject: any = {}
+  variableNames.map((name, index) => {
+    createdObject[name] = variableValues[index]
+  })
+  return createdObject
+}
 
 const ApplicationView: React.FC<ApplicationViewProps> = ({
   templateElement,
@@ -12,11 +43,14 @@ const ApplicationView: React.FC<ApplicationViewProps> = ({
   onSave,
 }) => {
   const [value, setValue] = useState(initialValue?.text)
+  const [dynamicValues, setDynamicValues] = useState({})
   const { placeholder, maskedInput, label } = templateElement?.parameters
 
   useEffect(() => {
     onUpdate(value)
   }, [])
+
+  evaluateDynamicFields(templateElement.parameters).then((res) => console.log('Evaluation:', res))
 
   function handleChange(e: any) {
     onUpdate(e.target.value)
