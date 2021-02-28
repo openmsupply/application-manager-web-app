e66196158a548853f08f6d25229865524f1d170e
andreievg <andrei.evguenov@gmail.com>
2021-02-28 14:02:30 +1300

    stash commit

--- src/components/List/Cells/StatusCell.tsx
+++ src/components/List/Cells/StatusCell.tsx
@@ -12,6 +12,52 @@ enum ACTIONS {
 
 const StatusCell: React.FC<CellProps> = ({ application }) => {
   const { serial, stage, status } = application
+
+  const reviewerActionables = []
+  const actionableList = [
+    {
+      icon: 'reply',
+      tooltip: 'Applications that can be slef assigned',
+      applicationKey: 'numberOfSelfAssignableReviews',
+    },
+    {
+      icon: 'reply',
+      tooltip: 'Applications that can be slef assigned',
+      applicationKey: 'numberOfDraftReviews',
+    },
+    {
+      icon: 'reply',
+      tooltip: 'Applications that can be slef assigned',
+      applicationKey: 'numberOfChangesRequestedReviews',
+    },
+    {
+      icon: 'reply',
+      tooltip: 'Applications that can be slef assigned',
+      applicationKey: 'numberOfAssignedNotStartedReviews',
+    },
+  ]
+
+  actionableList.forEach((actionable) => {
+    if (application[actionable.applicationKey] > 0) {
+      reviewerActionables.push(
+        <Label>
+          <Icon name={actionable.icon} />
+          {application[actionable.applicationKey]}
+        </Label>
+      )
+    }
+  })
+
+  if (application.numberOfSelfAssignableReviews > 0) {
+    return (
+      <Segment basic textAlign="center">
+        <Progress size="tiny" />
+        <Link to={`/application/${serial}`}></Link>
+        {/* <Icon name="trash alternate outline" style={{ marginLeft: 10 }} /> */}
+      </Segment>
+    )
+  }
+
   switch (status) {
     case ApplicationStatus.ChangesRequired:
       return (
