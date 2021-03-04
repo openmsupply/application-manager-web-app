441865cab84f509ab7ac9927643cb3d2d73686f5
andreievg <andrei.evguenov@gmail.com>
2021-03-04 22:39:26 +1300

    fix test

--- src/containers/Review/ReviewPageWrapperTest.tsx
+++ src/containers/Review/ReviewPageWrapperTest.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
 import { Loading } from '../../components'
-import { ReviewAssignment } from '../../utils/generated/graphql'
 import useGetFullReviewStructure from '../../utils/hooks/useGetFullReviewStructure'
 import { FullStructure } from '../../utils/types'
 
 const ReviewPageWrapperTest: React.FC<{ structure: FullStructure }> = ({ structure }) => {
-  const reviewAssignment: ReviewAssignment = {
-    id: 1007,
-  }
+  const reviewAssignmentId = 1007
 
-  return <ReviewPageTest structure={structure} reviewAssignment={reviewAssignment} />
+  return <ReviewPageTest structure={structure} reviewAssignmentId={reviewAssignmentId} />
 }
 
 const ReviewPageTest: React.FC<{
-  reviewAssignment: ReviewAssignment
+  reviewAssignmentId: number
   structure: FullStructure
-}> = ({ reviewAssignment, structure }) => {
-  const { fullStructure, error } = useGetFullReviewStructure({ reviewAssignment, structure })
+}> = ({ reviewAssignmentId, structure }) => {
+  const { fullStructure, error } = useGetFullReviewStructure({ reviewAssignmentId, structure })
 
   if (error) return <Header>Error</Header>
   if (!fullStructure) return <Loading />
