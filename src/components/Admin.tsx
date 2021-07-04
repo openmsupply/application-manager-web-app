d855965a94173793c48e338b2ae502929f0ca183
andreievg <andrei.evguenov@gmail.com>
2021-07-05 00:54:50 +1200

    admin menue

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Header } from 'semantic-ui-react'
 
 const Admin: React.FC = () => {
   return (
-    <div>
-      <h1>Admin area</h1>
-      <p>Only Admins can see this page.</p>
+    <div id="outcomes-display">
+      <Header as="h4">Admin</Header>
+      <div className="outcomes-container">
+        <Link className="clickable" to={`/admin/templates`}>
+          <div className="outcome">
+            <Header as="h3" className="clickable">
+              {`Templates/Procedures and Builder`}
+            </Header>
+          </div>
+        </Link>
+        <Link className="clickable" to={`/lookup-tables`}>
+          <div className="outcome">
+            <Header as="h3" className="clickable">
+              {`Lookup Tables`}
+            </Header>
+          </div>
+        </Link>
+        <Link className="clickable" to={`/admin/outcomes`}>
+          <div className="outcome">
+            <Header as="h3" className="clickable">
+              {`Outcome Configurations`}
+            </Header>
+          </div>
+        </Link>
+        <Link className="clickable" to={`/admin/permissions`}>
+          <div className="outcome">
+            <Header as="h3" className="clickable">
+              {`Permission Policies and Names`}
+            </Header>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
